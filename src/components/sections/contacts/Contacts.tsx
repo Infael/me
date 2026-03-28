a5847a051d9fb63cb0859b88c158218f9ac884eb
Michal Štefaňák <michalstefanak22@gmail.com>
2026-03-28 22:35:58 +0100

    feat: replaced social media wheel with individual icons

--- src/components/sections/contacts/Contacts.tsx
+++ src/components/sections/contacts/Contacts.tsx
@@ -1,8 +1,17 @@
 import { FC } from 'react';
 
 import { Header } from '../../header/Header';
-import { GeoIcon, MailIcon, PhoneIcon } from './components/ContactsIcons';
-import { SocialMediaWheel } from './components/socialMediaWheel/SocialMediaWheel';
+import {
+  GeoIcon,
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  MailIcon,
+  PhoneIcon,
+} from './components/ContactsIcons';
+import { SocialMediaIcon } from './components/socialMediaIcon/SocialMediaIcon';
+
+import moxfieldLogo from './components/moxfieldLogo.png';
 
 import styles from './Contacts.module.css';
 
@@ -25,7 +34,31 @@ export const Contacts: FC = () => {
             michalstefanak22@gmail.com
           </a>
         </div>
-        <SocialMediaWheel />
+        <div className={styles.socialMediaRow}>
+          <p>Find me also on</p>
+          <div className={styles.contactsRow}>
+            <SocialMediaIcon
+              href="https://github.com/Infael"
+              label="Github"
+              icon={<GitHubIcon />}
+            />
+            <SocialMediaIcon
+              href="https://cz.linkedin.com/in/michal-%C5%A1tefa%C5%88%C3%A1k"
+              label="LinkedIn"
+              icon={<LinkedInIcon />}
+            />
+            <SocialMediaIcon
+              href="https://www.instagram.com/michalmiskostefanak/"
+              label="Instagram"
+              icon={<InstagramIcon />}
+            />
+            <SocialMediaIcon
+              href="https://moxfield.com/users/Infael"
+              label="Moxfield"
+              icon={<img src={moxfieldLogo} alt="Moxfield" />}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
