834f9787ad11c14205f783a47d95c08117f847bb
Michal Štefaňák <61191393+Infael@users.noreply.github.com>
2026-03-29 14:38:58 +0200

    feat: replaced social media wheel with individual icons (#16)

---
+++ src/components/sections/contacts/components/socialMediaIcon/SocialMediaIcon.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+
+import styles from './SocialMediaIcon.module.css';
+
+interface SocialMediaIconProps {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+}
+
+export const SocialMediaIcon: FC<SocialMediaIconProps> = ({
+  href,
+  label,
+  icon,
+}) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.iconWrapper}
+    >
+      {icon}
+      <span className={styles.iconLabel}>{label}</span>
+
+      <div className={styles.hoverEffect}>
+        {icon}
+        <span className={styles.iconLabel}>{label}</span>
+      </div>
+    </a>
+  );
+};
